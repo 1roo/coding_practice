b51154570106847eac1f5bca5a8c70cdce793a94
hanaru <133945249+1roo@users.noreply.github.com>
2024-02-14 15:39:11 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,22 @@
+function solution(n) {
+    var toThree = '';
+    while (n > 0) {
+        toThree = n % 3 + toThree;
+        n = Math.floor(n / 3);
+    }
+    if (toThree === '') {
+        toThree = '0';
+    }
+
+    toThree = toThree.split('').reverse().join('');
+
+    
+    let toTen = 0;
+    for (let i = toThree.length - 1; i >= 0; i--) {
+        toTen += parseInt(toThree[i]) * Math.pow(3, toThree.length - 1 - i);
+    }
+    
+    
+    return toTen;
+}
+
