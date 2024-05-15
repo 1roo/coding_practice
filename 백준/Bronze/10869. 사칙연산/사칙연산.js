9cc76806aa15aafa30b9b3d7b9c0e44f185aaa99
hanaru <133945249+1roo@users.noreply.github.com>
2024-05-16 07:18:34 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 9584 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const [A, B] = fs.readFileSync(0, 'utf-8').toString().trim().split(' ').map(Number);
+
+console.log(`${A + B}
+${A - B}
+${A * B}
+${Math.trunc(A / B)}
+${A % B}`);
