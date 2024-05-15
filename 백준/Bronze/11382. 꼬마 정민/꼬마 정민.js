b117f3fca6754476ce3cdf5b00cd469deba4a509
hanaru <133945249+1roo@users.noreply.github.com>
2024-05-16 07:40:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+
+const [A, B, C] = fs.readFileSync(filePath).toString().trim().split(' ').map(Number);
+
+console.log(`${A+B+C}`);
