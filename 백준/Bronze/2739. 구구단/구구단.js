c3f59deba4a9471abff84b06b8e50eb983c60413
hanaru <133945249+1roo@users.noreply.github.com>
2024-05-16 07:53:32 +0900

    [Bronze V] Title: 구구단, Time: 124 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const filePath = process.platform === "linux" ? "dev/stdin" : "input.txt";
+
+const input = fs.readFileSync(filePath).toString().trim().split(' ');
+
+for(let i = 1; i < 10; i++) {
+    console.log(input+' * '+i+' = '+input*i);
+}
+
