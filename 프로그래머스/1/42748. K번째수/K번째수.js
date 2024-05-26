7b801811b3c79e113859621a06bac0d793f5e459
hanaru <133945249+1roo@users.noreply.github.com>
2024-05-26 19:34:23 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,17 +1,5 @@
 function solution(array, commands) {
-    const answer = [];
-    for(let i = 0; i < commands.length; i++) {
-        let arr1;
-        if(commands[i][0]===commands[i][1]) {
-            arr1 = array[commands[i][0]-1];
-            answer.push(arr1);
-        } else {
-            arr1 = array.slice(commands[i][0]-1, commands[i][1]);
-            arr1.sort((a, b) => a - b);
-            answer.push(arr1[commands[i][2]-1]);
-        }
-
-    }
-        
-    return answer;
+    return commands.map(([i, j, k]) => {
+        return array.slice(i - 1, j).sort((a, b) => a - b)[k - 1];
+    });
 }
